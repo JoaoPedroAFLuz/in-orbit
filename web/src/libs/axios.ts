27613b1c608f14e0eae50a389d2eb25e798718c4
João Pedro Fortunato <70025328+JoaoPedroAFLuz@users.noreply.github.com>
2024-09-14 19:00:00 -0300

    removido sleep das requisicoes

--- web/src/libs/axios.ts
+++ web/src/libs/axios.ts
@@ -5,8 +5,3 @@ import { env } from '../env';
 export const httpClient = axios.create({
   baseURL: env.VITE_API_BASE_URL,
 });
-
-httpClient.interceptors.response.use(async response => {
-  await new Promise(resolve => setTimeout(resolve, 1000));
-  return response;
-});
